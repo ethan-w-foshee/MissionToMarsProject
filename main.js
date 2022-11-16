4382361ff3256081aa7b58f0c625a058f69e5633
ethan.w.foshee <skyhailer2@gmail.com>
2022-11-15 20:03:03 -0600

    Constructors created

--- main.js
+++ main.js
@@ -10,10 +10,35 @@ const jobTypes = {
 };
 
 // Your code will go here
+class CrewMember {
+  constructor(name, job, specialSkill, ship){
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = ship;
+  }
+  enterShip(ship) {
+    this.ship = ship;
+    this.ship.crew.push(this);
+  }
+}
 
-
-
-
+class Ship {
+  constructor(name, type, ability) {
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+  }
+  missionStatement() {
+    if (this.crew.length == 0) {
+      return "Can't perform a mission yet."
+    }
+    else{
+      return this.ability
+    }
+  }
+}
 
 
 
